995bceb48d3283ff37559406f1097fdd0451e848
LapisIsCool <71887664+LapisIsCool@users.noreply.github.com>
2021-10-12 16:58:41 -0400

    Add files via upload

---
+++ apex project/random-gun.py
@@ -0,0 +1,70 @@
+import random
+
+randomNum = random.randint(1,32)
+# print(randomNum)
+print("You should use a ")
+
+if randomNum == 1:
+    print("Havoc")
+elif randomNum == 2:
+    print("Flatline")
+elif randomNum == 3:
+    print("Hemlok")
+elif randomNum == 4:
+    print("R-301")
+elif randomNum == 5:
+    print("Alternator")
+elif randomNum == 6:
+    print("Prowler")
+elif randomNum == 7:
+    print("R-99")
+elif randomNum == 8:
+    print("Volt")
+elif randomNum == 9:
+    print("Devotion")
+elif randomNum == 10:
+    print("L-star")
+elif randomNum == 11:
+    print("Spitfire")
+elif randomNum == 12:
+    print("Rampage")
+elif randomNum == 13:
+    print("G7 scout")
+elif randomNum == 14:
+    print("Triple take")
+elif randomNum == 15:
+    print("30-30 repeater")
+elif randomNum == 16:
+    print("Bocek")
+elif randomNum == 17:
+    print("Charge rifle")
+elif randomNum == 18:
+    print("Longbow")
+elif randomNum == 19:
+    print("Kraber")
+elif randomNum == 20:
+    print("Sentinel")
+elif randomNum == 21:
+    print("Eva-8 auto")
+elif randomNum == 22:
+    print("Mastiff")
+elif randomNum == 23:
+    print("Mozambique here")
+elif randomNum == 24:
+    print("Peacekeeper")
+elif randomNum == 25:
+    print("Re-45")
+elif randomNum == 26:
+    print("P2020")
+elif randomNum == 27:
+    print("Wingman")
+elif randomNum == 28:
+    print("First and second gun you see")
+elif randomNum == 29:
+    print("Only first gun you see")
+elif randomNum == 30:
+    print("One gun and reroll for the one gun")
+elif randomNum == 31:
+    print("Grenades only")
+elif randomNum == 32:
+    print("Fists only")
---
+++ apex project/random-legend.py
@@ -0,0 +1,42 @@
+import random
+
+randomNum = random.randint(1,18)
+# print(randomNum)
+print("You should play as ")
+
+if randomNum == 1:
+    print("Bloodhound")
+elif randomNum == 2:
+    print("Gibraltar")
+elif randomNum == 3:
+    print("Lifeline")
+elif randomNum == 4:
+    print("Pathfinder")
+elif randomNum == 5:
+    print("Wraith")
+elif randomNum == 6:
+    print("Bangalore")
+elif randomNum == 7:
+    print("Caustic")
+elif randomNum == 8:
+    print("Mirage")
+elif randomNum == 9:
+    print("Octane")
+elif randomNum == 10:
+    print("Wattson")
+elif randomNum == 11:
+    print("Crypto")
+elif randomNum == 12:
+    print("Revenant")
+elif randomNum == 13:
+    print("Loba")
+elif randomNum == 14:
+    print("Rampart")
+elif randomNum == 15:
+    print("Horizon")
+elif randomNum == 16:
+    print("Fuse")
+elif randomNum == 17:
+    print("Valkyrie")
+elif randomNum == 18:
+    print("Seer")
